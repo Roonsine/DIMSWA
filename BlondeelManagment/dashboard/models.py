19d18cc634804b510f15a2e20f08a839664ba664
roonsine <tcarron@teksavvy.com>
2023-06-25 19:52:44 -0400

    Added in Roonsine user, and access to admin page to login. Added Product Model

--- BlondeelManagment/dashboard/models.py
+++ BlondeelManagment/dashboard/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
+markup = 0.25
+
+
+def price(cost):
+    """
+    Takes in a cost and returns the marked up price of the item.
+    """
+    increase = markup * cost
+    actual_cost = increase + cost
+    return actual_cost
+
+
 # Create your models here.
+CATEGORY = (
+    ('Stationary', 'Stationary'),
+    ('Electronics', 'Electronics'),
+    ('Food', 'Food'),
+)
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=20, choices=CATEGORY)
+    product_id = models.CharField(max_length=25)
+    cost = models.FloatField()
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'{self.name} | {self.product_id} | {self.cost} | {self.quantity}'
